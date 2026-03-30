bf4ea1ce0f63bb88c1bce9ca6789c2d8acb1d808
codigoinerte <fmartinez.bpe@gmail.com>
2026-03-29 20:22:36 -0500

    fix: corrección de base en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss(), basicSsl({
     
   })],
