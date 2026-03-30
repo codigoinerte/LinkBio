768cee5a08b7ce0932d6474eba68d393adf207b2
codigoinerte <fmartinez.bpe@gmail.com>
2026-03-29 20:26:29 -0500

    fix: se removio ssl de vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss(), basicSsl({
-    
-  })],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
