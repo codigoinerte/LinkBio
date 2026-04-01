f33522a16e3ca242d98bcfe124b58b1e93b974c4
codigoinerte <fmartinez.bpe@gmail.com>
2026-04-01 09:46:43 -0500

    fix: corrección de enlaces desde login y cambio de initials

--- src/front/components/DropdownAuth.tsx
+++ src/front/components/DropdownAuth.tsx
@@ -1,10 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 import { useUserStore } from "@/context/userContext";
 import { routes } from "@/helpers/routes-list";
 
 import { Menu } from "lucide-react";
+import { useMemo } from "react";
 import { useNavigate } from "react-router";
 
 export const DropdownAuth = () => {
@@ -12,6 +13,13 @@ export const DropdownAuth = () => {
     const user = useUserStore(state => state.user);
 
     const navigate = useNavigate();
+
+    const initials = useMemo(()=> {
+        if(user?.name){
+            return user.name.split(" ").map(word => word[0]).join("");
+        }
+        return "CM";        
+    }, [user?.name])
 
     return (
         <DropdownMenu>
@@ -21,7 +29,9 @@ export const DropdownAuth = () => {
                     <div className="flex flex-row gap-2 cursor-pointer">
                         <Avatar className="h-8 w-8 rounded-lg">
                             <AvatarImage src={user?.photo ?? ""} alt={user?.name} />
-                            <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                            <AvatarFallback className="rounded-lg">
+                                {initials}
+                            </AvatarFallback>
                         </Avatar>
                         
                         <Menu className="ml-auto size-4 self-center"/>
@@ -34,22 +44,22 @@ export const DropdownAuth = () => {
                     <DropdownMenuGroup>
                         <DropdownMenuItem className="cursor-pointer" onClick={()=> navigate(routes.admin) }>
                             Dashboard
-                            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
                         </DropdownMenuItem>
                         <DropdownMenuItem className="cursor-pointer" onClick={()=> navigate(routes.profile)}>
                             Profile
-                            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
                         </DropdownMenuItem>
                         <DropdownMenuItem className="cursor-pointer" onClick={()=> navigate(routes.settings)}>
-                            Settings
-                            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                            Design
+                            {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
                         </DropdownMenuItem>
                         
                     </DropdownMenuGroup>                
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={logout}>                    
                     Log out
-                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
